84819c74384a4e6428fe5c9d49e9ccc3b21eeea1
clumsyyyy <owencwijaya@gmail.com>
2021-09-23 16:39:51 +0700

    SPL pake cramer

---
+++ src/SPLCramer.java
@@ -0,0 +1,25 @@
+public class SPLCramer {
+    static void Cramer(Matrix m){
+        Matrix tempMat = new Matrix(m.ROWS, m.COLS - 1);
+        Matrix cutM = new Matrix(m.ROWS, m.COLS - 1);
+        for(int k = 0; k < tempMat.ROWS; k++){
+            for(int l = 0; l < tempMat.COLS; l++){
+                tempMat.set(k, l, m.e(k, l));
+                cutM.set(k, l, m.e(k, l));
+            }
+        }
+
+        for(int i = 0; i < tempMat.ROWS; i++){
+            for(int j = 0; j < tempMat.COLS; j++){
+                tempMat.set(j, i, m.e(j, m.COLS - 1));
+            }
+            System.out.printf("x%d = ", i + 1);
+            System.out.printf("%.2f\n", Det.determinanKofaktor(tempMat) / Det.determinanKofaktor(cutM));
+            for(int k = 0; k < tempMat.ROWS; k++){
+                for(int l = 0; l < tempMat.COLS; l++){
+                    tempMat.set(k, l, m.e(k, l));
+                }
+            }
+        }
+    }
+}
