6a835b119315b9c3681fa20b50c4f64ec1f37a54
clumsyyyy <owencwijaya@gmail.com>
2021-09-24 10:39:23 +0700

    nambahin dialog

--- src/interPolinom.java
+++ src/interPolinom.java
@@ -26,19 +26,39 @@ public class interPolinom {
         }
         System.out.println("\nMatriks augmented persamaan: ");
         m.displayMatrix();
+        System.out.println("\nDitemukan nilai konstanta menggunakan SPL Cramer: ");
 
         float array[] = SPLCramer.Cramer(m);
         float approx = 0;
+
+        System.out.print("\nPersamaan polinom: ");
+        for(int i = 0; i < size; i++){
+            System.out.printf("%f", array[i]);
+            if(i != 0){
+                System.out.printf("x^%d", i);
+            }
+            if(i + 1 != size){
+                if(array[i + 1] > 0 && i < size){
+                    System.out.print(" + ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            
+        }
+
+
+
         System.out.print("\nMasukkan nilai x yang akan diaproksimasi: ");
         approx = sc.nextFloat();
         float result = array[0];
-
+        float tempApprox = approx;
         for (int i = 1; i < size; i++){
-            result = result + approx * array[i];
-            approx = approx * approx;
+            result = result + tempApprox * array[i];
+            tempApprox = tempApprox * approx;
         }
 
-    
+        System.out.printf("Aproksimasi nilai %f terhadap interpolasi polinom: ", approx);
         System.out.println(result);
         sc.close();
     }
