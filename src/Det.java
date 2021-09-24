7796e36676752fd38ec33c31586c580a10269c4f
clumsyyyy <owencwijaya@gmail.com>
2021-09-24 10:55:27 +0700

    bugfix buat gaussian

--- src/Det.java
+++ src/Det.java
@@ -1,21 +1,20 @@
 public class Det{
 
-    //n = ukuran matriks
-    static float determinanKofaktor(Matrix m, int n){
+    public static float determinanKofaktor(Matrix m){
         float det = 0;
         int rowCount = 0;
         int colCount = 0;
-        Matrix temp = new Matrix(n - 1, n - 1);
-        if (n == 2){
+        Matrix temp = new Matrix(m.ROWS - 1, m.COLS  - 1);
+        if (m.ROWS  == 2){
             return (m.e(0, 0) * m.e(1, 1) - m.e(1, 0) * m.e(0, 1)); 
         } else {
             det = 0;
-            for(int i = 0; i < n; i++){
+            for(int i = 0; i < m.ROWS ; i++){
                 if(m.e(0, 1) != 0){
                     rowCount = 0;
-                    for(int j = 0; j < n; j++){
+                    for(int j = 0; j < m.ROWS ; j++){
                         colCount = 0;
-                        for(int k = 0; k < n; k++){
+                        for(int k = 0; k < m.ROWS; k++){
                             if (k != i){
                                 temp.set(rowCount, colCount, m.e(j, k));
                                 colCount = colCount + 1;
@@ -25,11 +24,10 @@ public class Det{
                             rowCount = rowCount + 1;
                         }
                     }
-                    
                     if (i % 2 != 0){
-                        det = det - m.e(0, i) * determinanKofaktor(temp, n - 1);
+                        det = det - m.e(0, i) * determinanKofaktor(temp);
                     }else{
-                        det = det + m.e(0, i) * determinanKofaktor(temp, n - 1);
+                        det = det + m.e(0, i) * determinanKofaktor(temp);
                     }
                     
                 }
@@ -39,40 +37,45 @@ public class Det{
     }
  
 
-    static float determinanGaussian(Matrix m, int n){
+    public static float determinanGaussian(Matrix m){
         float det = 1;
-        float temp = 0;
+
         int count;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < m.ROWS ; i++){
             
             if (m.e(i, i) == 0){
                 count = 0;
-                while ((i + count < n) && m.e(i + count, i) == 0){
+                while ((i + count < m.ROWS ) && m.e(i + count, i) == 0){
                     count = count + 1;
                 }
 
-                for(int k = 0; k < n; k++){
-                    temp = m.e(i, k);
-                    m.set(i, k, m.e(i + count, k));
-                    m.set(i + count, k, temp);
+                for(int k = 0; k < m.ROWS ; k++){
+                    m.swap(i, k);
                 }
             }
             
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < m.ROWS ; j++){
                 if(i != j){
                     float comp = m.e(j, i) / m.e(i, i);
-                    for(int k = 0; k < n; k++){
+                    for(int k = 0; k < m.ROWS ; k++){
                         m.set(j, k, (m.e(j, k) - m.e(i, k) * comp));
                     }
                 }
             }
             m.displayMatrix();
         }
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < m.ROWS ; i++){
             det = det * m.e(i, i);
         }
         return det;
-        //masih ada bug?
+    }
+
+    public static void main(String[] args){
+        Matrix m = new Matrix(3, 3);
+        m.readMatrix();
+        System.out.println(determinanKofaktor(m));
+        System.out.println(determinanGaussian(m));
+        
     }
 }
 
