126de40ab14e3e0dad4da5cf9f36e3f5d0745d6d
clumsyyyy <owencwijaya@gmail.com>
2021-09-24 10:56:16 +0700

    return float untuk interpolinom

--- src/SPLCramer.java
+++ src/SPLCramer.java
@@ -1,5 +1,6 @@
 public class SPLCramer {
-    static void Cramer(Matrix m){
+    public static float[] Cramer(Matrix m){
+        float array[] = new float[m.ROWS];
         Matrix tempMat = new Matrix(m.ROWS, m.COLS - 1);
         Matrix cutM = new Matrix(m.ROWS, m.COLS - 1);
         for(int k = 0; k < tempMat.ROWS; k++){
@@ -12,14 +13,19 @@ public class SPLCramer {
         for(int i = 0; i < tempMat.ROWS; i++){
             for(int j = 0; j < tempMat.COLS; j++){
                 tempMat.set(j, i, m.e(j, m.COLS - 1));
+                
             }
             System.out.printf("x%d = ", i + 1);
             System.out.printf("%.2f\n", Det.determinanKofaktor(tempMat) / Det.determinanKofaktor(cutM));
+            array[i] = Det.determinanKofaktor(tempMat) / Det.determinanKofaktor(cutM);
             for(int k = 0; k < tempMat.ROWS; k++){
                 for(int l = 0; l < tempMat.COLS; l++){
                     tempMat.set(k, l, m.e(k, l));
                 }
             }
+    
         }
+        return array;
     }
+    
 }
