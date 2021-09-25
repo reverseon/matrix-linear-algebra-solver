767b0d4daa420fee3b0dfafaab102cfa35d4dff7
Dzakfr1006 <16520278@std.stei.itb.ac.id>
2021-09-25 15:03:04 +0700

    Added implementation for some classes. Check TODO

--- src/mainpr.java
+++ src/mainpr.java
@@ -1,34 +1,37 @@
 import java.util.Scanner;
 
 public class mainpr {
+    // TODO: Corner cases. Handling more than one action.
     public static void main(String[] args) {
         int input, exitcode;
+        Matrix m = new Matrix(100, 100);
         exitcode = 0;
         Scanner sc = new Scanner(System.in);
 
         while (exitcode == 0) {
             mainMenu();
             input = -1;
-            do {
-                input = sc.nextInt();
-            } while (input < 0 || input > 6);
-
+            input(sc, input, 0, 6);
             switch (input) {
                 case 1:
                     System.out.println("1 Selected.");
-                    subMenu1(sc);
+                    splMenu(sc, m);
                     break;
                 case 2:
                     System.out.println("2 Selected.");
+                    detMenu(sc, m);
                     break;
                 case 3:
                     System.out.println("3 Selected.");
+                    matBalik(sc, m);
                     break;
                 case 4:
                     System.out.println("4 Selected.");
+                    InterpolasiPolinom.intPol();
                     break;
                 case 5:
                     System.out.println("5 Selected.");
+                    doubleLinReg(sc, m);
                     break;
                 case 6:
                     System.out.println("Exiting...");
@@ -52,32 +55,136 @@ public class mainpr {
         System.out.println("Masukkan pilihan menu (1/2/3/4/5/6/0) : ");
     }
 
-    public static void subMenu1(Scanner sc) {
-        int input;
+    // TODO: Actual implementation of SPLxx.java for each option bcuz idfk how to
+    // use it
+    public static void splMenu(Scanner sc, Matrix m) {
+        int input = 0, ipt1 = 0;
+        int result;
         System.out.println("\nSISTEM PERSAMAAN LINIER \n[1] Metode eliminasi Gauss\n[2] Metode eliminasi Gauss-Jordan");
         System.out.println("[3] Metode matriks balikan\n[4] Kaidah Cramer\n[5] Kembali");
         System.out.println("Masukkan pilihan menu (1/2/3/4/5) : ");
-        do {
-            input = sc.nextInt();
-        } while (input < 1 || input > 5);
+        input(sc, input, 1, 5);
 
         switch (input) {
             case 1:
                 System.out.println("1 Selected.");
+                System.out.println("Masukkan pilihan input [1] keyboard | [2] file .txt (1/2) : ");
+                input(sc, ipt1, 1, 2);
+                switch (ipt1) {
+                    case 1:
+                        m.ROWS = sc.nextInt();
+                        m.COLS = sc.nextInt();
+                        m.readMatrix(sc);
+                    case 2:
+                        ReadText.readtxt(m);
+                }
+                result = SPLGauss.solve(m);
+                System.out.println("Hasil perhitungan menggunakan metode eliminasi Gauss: ");
+                System.out.println(result);
                 break;
             case 2:
                 System.out.println("2 Selected.");
+                System.out.println("Masukkan pilihan input [1] keyboard | [2] file .txt (1/2) : ");
+                input(sc, ipt1, 1, 2);
+                switch (ipt1) {
+                    case 1:
+                        m.ROWS = sc.nextInt();
+                        m.COLS = sc.nextInt();
+                        m.readMatrix(sc);
+                    case 2:
+                        ReadText.readtxt(m);
+                }
+                result = SPLGaussJordan.solve(m);
+                System.out.println("Hasil perhitungan menggunakan metode eliminasi Gauss: ");
+                System.out.println(result);
                 break;
             case 3:
                 System.out.println("3 Selected.");
+                System.out.println("Masukkan pilihan input [1] keyboard | [2] file .txt (1/2) : ");
+                input(sc, ipt1, 1, 2);
+                switch (ipt1) {
+                    case 1:
+                        m.ROWS = sc.nextInt();
+                        m.COLS = sc.nextInt();
+                        m.readMatrix(sc);
+                    case 2:
+                        ReadText.readtxt(m);
+                }
+                result = 0;
+                System.out.println("Hasil perhitungan menggunakan metode eliminasi Gauss: ");
+                System.out.println(result);
                 break;
             case 4:
                 System.out.println("4 Selected.");
+                System.out.println("Masukkan pilihan input [1] keyboard | [2] file .txt (1/2) : ");
+                input(sc, ipt1, 1, 2);
+                switch (ipt1) {
+                    case 1:
+                        m.ROWS = sc.nextInt();
+                        m.COLS = sc.nextInt();
+                        m.readMatrix(sc);
+                    case 2:
+                        ReadText.readtxt(m);
+                }
+                result = 0;
+                System.out.println("Hasil perhitungan menggunakan metode eliminasi Gauss: ");
+                System.out.println(result);
                 break;
             case 5:
                 System.out.println("5 Selected.");
                 break;
         }
+    }
+
+    public static void detMenu(Scanner sc, Matrix m) {
+        int input = 0;
+        System.out.println("\nDETERMINAN");
+        System.out.println("Masukkan pilihan input [1] keyboard | [2] file .txt (1/2) : ");
+        input(sc, input, 1, 2);
+        switch (input) {
+            case 1:
+                m.ROWS = sc.nextInt();
+                m.COLS = m.ROWS;
+                m.readMatrix(sc);
+            case 2:
+                ReadText.readtxt(m);
+        }
+        System.out.println("Determinan dengan ekspansi kofaktor adalah " + Det.determinanKofaktor(m));
+        System.out.println("Determinan dengan reduksi baris adalah " + Det.determinanGaussian(m));
+    }
+
+    // TODO: Implementasi matBalik() (Matriks Balikan)
+    public static void matBalik(Scanner sc, Matrix m) {
+        int input = 0;
+        System.out.println("\nMATRIKS BALIKAN");
+        System.out.println("Masukkan pilihan input [1] keyboard | [2] file .txt (1/2) : ");
+        input(sc, input, 1, 2);
+        switch (input) {
+            case 1:
+                System.out.print("Jumlah Titik: ");
+                m.ROWS = sc.nextInt();
+                m.COLS = m.ROWS;
+                m.readMatrix(sc);
+            case 2:
+                ReadText.readtxt(m);
+        }
+    }
+
+    public static void doubleLinReg(Scanner sc, Matrix m) {
+        int input = 0;
+        System.out.println("\n REGRESI LINIER BERGANDA");
+        System.out.println("Masukkan pilihan input [1] keyboard | [2] file .txt (1/2) : ");
+        input(sc, input, 1, 2);
+        switch (input) {
+            case 1:
+                System.out.print("Jumlah Titik: ");
+                m.ROWS = sc.nextInt();
+                m.COLS = m.ROWS;
+                m.readMatrix(sc);
+            case 2:
+                ReadText.readtxt(m);
+        }
+        DoubleLinReg.solve(m, sc);
     }
 
     public static void debug(Scanner sc) {
@@ -86,5 +193,11 @@ public class mainpr {
         m1.set(1, 3, 12);
         m1.displayMatrix();
         System.out.println("");
+    }
+
+    public static void input(Scanner sc, int ipt, int fOpt, int lOpt) {
+        do {
+            ipt = sc.nextInt();
+        } while (ipt < fOpt || ipt > lOpt);
     }
 }
