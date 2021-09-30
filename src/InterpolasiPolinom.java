2def748ac4975f6c88ddd9a7b46e254ef1a13c90
clumsyyyy <71161031+clumsyyyy@users.noreply.github.com>
2021-09-30 19:35:13 +0700

    improved file input handling

--- src/InterpolasiPolinom.java
+++ src/InterpolasiPolinom.java
@@ -4,23 +4,45 @@ public class InterpolasiPolinom {
     public static void intPol(Scanner sc) {
         String iptstr = "", strOut = "Matriks augmented: \n";
         int size = 0;
+        int ipt;
+        System.out.println("\nMasukkan pilihan input [1] keyboard | [2] file .txt (1/2) : ");
+        ipt = sc.nextInt();
+        MatrixDouble m = new MatrixDouble(100, 100);
+        switch (ipt) {
+            case 1:
+                System.out.print("Masukkan banyaknya pasangan titik: ");
+                size = sc.nextInt();
+                m.ROWS = size;
+                m.COLS = size + 1;
+                System.out.println("\nMasukkan titik (x, y): ");
+                for (int i = 0; i < size; i++) {
+                    double val = 1;
+                    double x = sc.nextDouble();
+                    double y = sc.nextDouble();
 
-        System.out.print("Masukkan banyaknya pasangan titik: ");
-        size = sc.nextInt();
-        MatrixDouble m = new MatrixDouble(size, size + 1);
-
-        System.out.println("\nMasukkan titik (x, y): ");
-        for (int i = 0; i < size; i++) {
-            double val = 1;
-            double x = sc.nextDouble();
-            double y = sc.nextDouble();
-
-            for (int j = 0; j < size; j++) {
-                m.set(i, j, val);
-                val = val * x;
-            }
-            m.set(i, size, y);
+                    for (int j = 0; j < size; j++) {
+                        m.set(i, j, val);
+                        val = val * x;
+                    }
+                    m.set(i, size, y);
+                }
+                break;
+            case 2:
+                MatrixDouble tempMatrix = new MatrixDouble(100, 100);
+                ReadWriteText.readtxtD(tempMatrix, sc);
+                m.ROWS = tempMatrix.ROWS;
+                m.COLS = tempMatrix.ROWS + 1;
+                for (int i = 0; i < tempMatrix.ROWS; i++) {
+                    double dVal = 1;
+                    for (int j = 0; j < tempMatrix.ROWS; j++) {   
+                        m.set(i, j, dVal);
+                        dVal = dVal * tempMatrix.e(i, 0);
+                    }
+                    m.set(i, tempMatrix.ROWS, tempMatrix.e(i, 1));
+                }
+                break;
         }
+        
         System.out.println("\nMatriks augmented persamaan: ");
         m.displayMatrix();
         strOut = strOut.concat(ReadWriteText.matrixToStrD(m));
@@ -28,12 +50,12 @@ public class InterpolasiPolinom {
         m.displayAsEqn();
 
         double array[] = new double[m.ROWS];
-        for (int i = 0; i < size; i++) {
-            array[i] = m.e(i, size);
+        for (int i = 0; i < m.ROWS; i++) {
+            array[i] = m.e(i, m.ROWS);
         }
         System.out.print("\nPersamaan polinom: ");
         strOut = strOut.concat("Persamaan polinom: ");
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < m.ROWS; i++) {
 
             System.out.printf("%f", array[i]);
             strOut = strOut.concat(Double.toString(array[i]));
@@ -41,8 +63,8 @@ public class InterpolasiPolinom {
                 System.out.printf("x^%d", i);
                 strOut = strOut.concat("x^" + Integer.toString(i));
             }
-            if (i + 1 != size) {
-                if (array[i + 1] > 0 && i < size) {
+            if (i + 1 != m.ROWS) {
+                if (array[i + 1] > 0 && i <m.ROWS) {
                     System.out.print(" + ");
                     strOut = strOut.concat(" + ");
                 } else {
@@ -61,7 +83,7 @@ public class InterpolasiPolinom {
             if (approx != -999) {
                 countX += 1;
                 double result = array[0];
-                for (int i = 1; i < size; i++) {
+                for (int i = 1; i < m.ROWS; i++) {
                     result = result + (double) (Math.pow(approx, i) * array[i]);
                 }
                 System.out.printf("Aproksimasi nilai %f terhadap interpolasi polinom: ", approx);
