fb20244f5915e2be1b047035e7b4f5a5edb9b631
clumsyyyy <71161031+clumsyyyy@users.noreply.github.com>
2021-09-30 19:37:09 +0700

    added another double handler

--- src/ReadWriteText.java
+++ src/ReadWriteText.java
@@ -44,6 +44,46 @@ public class ReadWriteText {
         }
     }
 
+    public static void readtxtD(MatrixDouble m, Scanner sc) {
+        String fileName;
+        fileName = sc.nextLine();
+        do {
+            System.out.println("Masukkan nama file (<namafile>.txt): ");
+            fileName = sc.nextLine();
+        } while (fileName == "");
+        System.out.println("File name selected: " + fileName);
+        try {
+
+            File currDir = new File("..");
+            File txt = new File(currDir, "test\\" + fileName);
+            Scanner sizeReader = new Scanner(txt);
+            int rowSize = 0;
+            int i = 0, j = 0;
+            while (sizeReader.hasNextLine()) {
+                rowSize++;
+                sizeReader.nextLine();
+            }
+            sizeReader.close();
+            m.ROWS = rowSize;
+            Scanner reader = new Scanner(txt);
+            try {
+                for (i = 0; i < m.ROWS; i++) {
+                    String line = reader.nextLine();
+                    String[] row = line.split(" ");
+                    m.COLS = row.length;
+                    for (j = 0; j < m.COLS; j++) {
+                        m.set(i, j, Double.parseDouble(row[j]));
+                    }
+                }
+            } finally {
+                reader.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            // e.printStackTrace();
+        }
+    }
+
     public static void writetxt(String str, Scanner sc) {
         String fileOut;
         do {
